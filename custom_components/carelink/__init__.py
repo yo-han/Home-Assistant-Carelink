55072ea57789631157aff4a16692f621684cba85
Yo-han <197600+yo-han@users.noreply.github.com>
2023-01-04 19:11:31 +0100

    fix: Lost connection fallback fix (#11)

--- custom_components/carelink/__init__.py
+++ custom_components/carelink/__init__.py
@@ -22,7 +22,6 @@ from .const import (
     DEVICE_PUMP_MODEL,
     DEVICE_PUMP_NAME,
     DEVICE_PUMP_SERIAL,
-    DOMAIN,
     SENSOR_KEY_PUMP_BATTERY_LEVEL,
     SENSOR_KEY_CONDUIT_BATTERY_LEVEL,
     SENSOR_KEY_SENSOR_BATTERY_LEVEL,
@@ -82,11 +81,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         config[CONF_USERNAME], config[CONF_PASSWORD], config["country"]
     )
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
-        CLIENT: carelink_client}
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {CLIENT: carelink_client}
 
-    coordinator = CarelinkCoordinator(
-        hass, entry, update_interval=SCAN_INTERVAL)
+    coordinator = CarelinkCoordinator(hass, entry, update_interval=SCAN_INTERVAL)
 
     await coordinator.async_config_entry_first_refresh()
 
@@ -126,15 +123,15 @@ class CarelinkCoordinator(DataUpdateCoordinator):
         await self.client.login()
         recent_data = await self.client.get_recent_data()
 
-        TIMEZONE_MAP = MS_TIMEZONE_TO_IANA_MAP.setdefault(recent_data.setdefault(
-            "clientTimeZoneName"), "Europe/London")
-        TIMEZONE = ZoneInfo(TIMEZONE_MAP)
+        timezone_map = MS_TIMEZONE_TO_IANA_MAP.setdefault(
+            recent_data.setdefault("clientTimeZoneName"), "Europe/London"
+        )
+        timezone = ZoneInfo(timezone_map)
 
         recent_data["lastSG"] = recent_data.setdefault("lastSG", {})
 
-        recent_data["activeInsulin"] = recent_data.setdefault(
-            "activeInsulin", {})
-        recent_data["basel"] = recent_data.setdefault("basel", {})
+        recent_data["activeInsulin"] = recent_data.setdefault("activeInsulin", {})
+        recent_data["basal"] = recent_data.setdefault("basal", {})
         recent_data["lastAlarm"] = recent_data.setdefault("lastAlarm", {})
         recent_data["markers"] = recent_data.setdefault("markers", [])
 
@@ -150,173 +147,223 @@ class CarelinkCoordinator(DataUpdateCoordinator):
             # Update glucose data only if data was logged. Otherwise, keep the old data and
             # update the latest sensor state because it probably changed to an error state
             if last_sg["sg"] > 0:
-                data[SENSOR_KEY_LASTSG_MMOL] = float(
-                    round(last_sg["sg"] * 0.0555, 2))
+                data[SENSOR_KEY_LASTSG_MMOL] = float(round(last_sg["sg"] * 0.0555, 2))
                 data[SENSOR_KEY_LASTSG_MGDL] = last_sg["sg"]
 
-            data[SENSOR_KEY_LASTSG_TIMESTAMP] = date_time_local.replace(
-                tzinfo=TIMEZONE)
+            data[SENSOR_KEY_LASTSG_TIMESTAMP] = date_time_local.replace(tzinfo=timezone)
         else:
-            data[SENSOR_KEY_LASTSG_MMOL] = None
-            data[SENSOR_KEY_LASTSG_MGDL] = None
-            data[SENSOR_KEY_LASTSG_TIMESTAMP] = None
+            data[SENSOR_KEY_LASTSG_MMOL] = UNAVAILABLE
+            data[SENSOR_KEY_LASTSG_MGDL] = UNAVAILABLE
+            data[SENSOR_KEY_LASTSG_TIMESTAMP] = UNAVAILABLE
 
         # Sensors
 
         data[SENSOR_KEY_PUMP_BATTERY_LEVEL] = recent_data.setdefault(
-            "medicalDeviceBatteryLevelPercent", UNAVAILABLE)
+            "medicalDeviceBatteryLevelPercent", UNAVAILABLE
+        )
         data[SENSOR_KEY_CONDUIT_BATTERY_LEVEL] = recent_data.setdefault(
-            "conduitBatteryLevel", UNAVAILABLE)
+            "conduitBatteryLevel", UNAVAILABLE
+        )
         data[SENSOR_KEY_SENSOR_BATTERY_LEVEL] = recent_data.setdefault(
-            "gstBatteryLevel", UNAVAILABLE)
+            "gstBatteryLevel", UNAVAILABLE
+        )
         data[SENSOR_KEY_SENSOR_DURATION_HOURS] = recent_data.setdefault(
-            "sensorDurationHours", UNAVAILABLE)
+            "sensorDurationHours", UNAVAILABLE
+        )
         data[SENSOR_KEY_SENSOR_DURATION_MINUTES] = recent_data.setdefault(
-            "sensorDurationMinutes", UNAVAILABLE)
+            "sensorDurationMinutes", UNAVAILABLE
+        )
         data[SENSOR_KEY_RESERVOIR_LEVEL] = recent_data.setdefault(
-            "reservoirLevelPercent", UNAVAILABLE)
+            "reservoirLevelPercent", UNAVAILABLE
+        )
         data[SENSOR_KEY_RESERVOIR_AMOUNT] = recent_data.setdefault(
-            "reservoirAmount", UNAVAILABLE)
+            "reservoirAmount", UNAVAILABLE
+        )
         data[SENSOR_KEY_RESERVOIR_REMAINING_UNITS] = recent_data.setdefault(
-            "reservoirRemainingUnits", UNAVAILABLE)
+            "reservoirRemainingUnits", UNAVAILABLE
+        )
         data[SENSOR_KEY_LASTSG_TREND] = recent_data.setdefault(
-            "lastSGTrend", UNAVAILABLE)
+            "lastSGTrend", UNAVAILABLE
+        )
 
         if "amount" in recent_data["activeInsulin"]:
             # Active insulin sensor
 
-            activeInsulin = recent_data["activeInsulin"]
-
-            date_time_local = datetime.strptime(
-                activeInsulin["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ"
-            ).replace(tzinfo=None)
+            active_insulin = recent_data["activeInsulin"]
 
             data[SENSOR_KEY_ACTIVE_INSULIN] = recent_data["activeInsulin"].setdefault(
-                "amount", UNAVAILABLE)
-            data[SENSOR_KEY_ACTIVE_INSULIN_ATTRS] = {
-                "last_update": date_time_local.replace(tzinfo=TIMEZONE)}
+                "amount", UNAVAILABLE
+            )
+
+            if "datetime" in active_insulin:
+                date_time_local = datetime.strptime(
+                    active_insulin["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).replace(tzinfo=None)
+                data[SENSOR_KEY_ACTIVE_INSULIN_ATTRS] = {
+                    "last_update": date_time_local.replace(tzinfo=timezone)
+                }
         else:
-            data[SENSOR_KEY_ACTIVE_INSULIN] = None
+            data[SENSOR_KEY_ACTIVE_INSULIN] = UNAVAILABLE
             data[SENSOR_KEY_ACTIVE_INSULIN_ATTRS] = {}
 
         if "datetime" in recent_data["lastAlarm"]:
             # Last alarm sensor
 
-            lastAlarm = recent_data["lastAlarm"]
+            last_alarm = recent_data["lastAlarm"]
 
             date_time_local = datetime.strptime(
-                lastAlarm["datetime"], "%Y-%m-%dT%H:%M:%S.000-00:00"
+                last_alarm["datetime"], "%Y-%m-%dT%H:%M:%S.000-00:00"
             ).replace(tzinfo=None)
 
-            data[SENSOR_KEY_LAST_ALARM] = date_time_local.replace(
-                tzinfo=TIMEZONE)
-            data[SENSOR_KEY_LAST_ALARM_ATTRS] = lastAlarm
+            data[SENSOR_KEY_LAST_ALARM] = date_time_local.replace(tzinfo=timezone)
+            data[SENSOR_KEY_LAST_ALARM_ATTRS] = last_alarm
         else:
             data[SENSOR_KEY_ACTIVE_INSULIN] = None
             data[SENSOR_KEY_ACTIVE_INSULIN_ATTRS] = {}
 
-        data[SENSOR_KEY_ACTIVE_BASAL_PATTERN] = recent_data["basal"].setdefault(
-            "activeBasalPattern", UNAVAILABLE)
+        if (
+            recent_data["basal"] is not None
+            and "activeBasalPattern" in recent_data["basal"]
+        ):
+            data[SENSOR_KEY_ACTIVE_BASAL_PATTERN] = recent_data["basal"].setdefault(
+                "activeBasalPattern", UNAVAILABLE
+            )
+        else:
+            data[SENSOR_KEY_ACTIVE_BASAL_PATTERN] = UNAVAILABLE
+
         data[SENSOR_KEY_AVG_GLUCOSE_MMOL] = float(
-            round(recent_data.setdefault(
-                "averageSG", UNAVAILABLE) * 0.0555, 2))
+            round(recent_data.setdefault("averageSG", UNAVAILABLE) * 0.0555, 2)
+        )
         data[SENSOR_KEY_AVG_GLUCOSE_MGDL] = recent_data.setdefault(
-            "averageSG", UNAVAILABLE)
+            "averageSG", UNAVAILABLE
+        )
         data[SENSOR_KEY_BELOW_HYPO_LIMIT] = recent_data.setdefault(
-            "belowHypoLimit", UNAVAILABLE)
+            "belowHypoLimit", UNAVAILABLE
+        )
         data[SENSOR_KEY_ABOVE_HYPER_LIMIT] = recent_data.setdefault(
-            "aboveHyperLimit", UNAVAILABLE)
+            "aboveHyperLimit", UNAVAILABLE
+        )
         data[SENSOR_KEY_TIME_IN_RANGE] = recent_data.setdefault(
-            "timeInRange", UNAVAILABLE)
+            "timeInRange", UNAVAILABLE
+        )
         data[SENSOR_KEY_MAX_AUTO_BASAL_RATE] = recent_data.setdefault(
-            "maxAutoBasalRate", UNAVAILABLE)
+            "maxAutoBasalRate", UNAVAILABLE
+        )
         data[SENSOR_KEY_SG_BELOW_LIMIT] = recent_data.setdefault(
-            "sgBelowLimit", UNAVAILABLE)
+            "sgBelowLimit", UNAVAILABLE
+        )
 
-        lastMealMarker = getLastMarker("MEAL", recent_data["markers"])
+        last_meal_marker = get_last_marker("MEAL", recent_data["markers"])
 
-        if lastMealMarker is not None:
-            data[SENSOR_KEY_LAST_MEAL_MARKER] = lastMealMarker["DATETIME"]
-            data[SENSOR_KEY_LAST_MEAL_MARKER_ATTRS] = lastMealMarker["ATTRS"]
+        if last_meal_marker is not None:
+            data[SENSOR_KEY_LAST_MEAL_MARKER] = last_meal_marker["DATETIME"]
+            data[SENSOR_KEY_LAST_MEAL_MARKER_ATTRS] = last_meal_marker["ATTRS"]
         else:
-            data[SENSOR_KEY_LAST_MEAL_MARKER] = None
+            data[SENSOR_KEY_LAST_MEAL_MARKER] = UNAVAILABLE
 
-        lastInsulineMarker = getLastMarker("INSULIN", recent_data["markers"])
+        last_insuline_marker = get_last_marker("INSULIN", recent_data["markers"])
 
-        if lastInsulineMarker is not None:
-            data[SENSOR_KEY_LAST_INSULIN_MARKER] = lastInsulineMarker["DATETIME"]
-            data[SENSOR_KEY_LAST_INSULIN_MARKER_ATTRS] = lastInsulineMarker["ATTRS"]
+        if last_insuline_marker is not None:
+            data[SENSOR_KEY_LAST_INSULIN_MARKER] = last_insuline_marker["DATETIME"]
+            data[SENSOR_KEY_LAST_INSULIN_MARKER_ATTRS] = last_insuline_marker["ATTRS"]
         else:
-            data[SENSOR_KEY_LAST_INSULIN_MARKER] = None
+            data[SENSOR_KEY_LAST_INSULIN_MARKER] = UNAVAILABLE
 
-        lastAutoBaselMarker = getLastMarker(
-            "AUTO_BASAL_DELIVERY", recent_data["markers"])
+        last_autobasal_marker = get_last_marker(
+            "AUTO_BASAL_DELIVERY", recent_data["markers"]
+        )
 
-        if lastAutoBaselMarker is not None:
-            data[SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER] = lastAutoBaselMarker["DATETIME"]
-            data[SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER_ATTRS] = lastAutoBaselMarker["ATTRS"]
+        if last_autobasal_marker is not None:
+            data[SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER] = last_autobasal_marker[
+                "DATETIME"
+            ]
+            data[
+                SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER_ATTRS
+            ] = last_autobasal_marker["ATTRS"]
         else:
-            data[SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER] = None
+            data[SENSOR_KEY_LAST_AUTO_BASAL_DELIVERY_MARKER] = UNAVAILABLE
 
-        lastAutoModeStatusMarker = getLastMarker(
-            "AUTO_MODE_STATUS", recent_data["markers"])
+        last_auto_mode_status_marker = get_last_marker(
+            "AUTO_MODE_STATUS", recent_data["markers"]
+        )
 
-        if lastAutoModeStatusMarker is not None:
-            data[SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER] = lastAutoModeStatusMarker["DATETIME"]
-            data[SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER_ATTRS] = lastAutoModeStatusMarker["ATTRS"]
+        if last_auto_mode_status_marker is not None:
+            data[
+                SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER
+            ] = last_auto_mode_status_marker["DATETIME"]
+            data[
+                SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER_ATTRS
+            ] = last_auto_mode_status_marker["ATTRS"]
         else:
-            data[SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER] = None
+            data[SENSOR_KEY_LAST_AUTO_MODE_STATUS_MARKER] = UNAVAILABLE
 
-        lastLowGlucoseMarker = getLastMarker(
-            "LOW_GLUCOSE_SUSPENDED", recent_data["markers"])
+        last_low_glucose_marker = get_last_marker(
+            "LOW_GLUCOSE_SUSPENDED", recent_data["markers"]
+        )
 
-        if lastLowGlucoseMarker is not None:
-            data[SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER] = lastLowGlucoseMarker["DATETIME"]
-            data[SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER_ATTRS] = lastAutoModeStatusMarker["ATTRS"]
+        if last_low_glucose_marker is not None:
+            data[
+                SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER
+            ] = last_low_glucose_marker["DATETIME"]
+            data[
+                SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER_ATTRS
+            ] = last_low_glucose_marker["ATTRS"]
         else:
-            data[SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER] = None
+            data[SENSOR_KEY_LAST_LOW_GLUCOSE_SUSPENDED_MARKER] = UNAVAILABLE
 
         # Binary Sensors
 
         data[BINARY_SENSOR_KEY_PUMP_COMM_STATE] = recent_data.setdefault(
-            "pumpCommunicationState", UNAVAILABLE)
+            "pumpCommunicationState", UNAVAILABLE
+        )
         data[BINARY_SENSOR_KEY_SENSOR_COMM_STATE] = recent_data.setdefault(
-            "gstCommunicationState", UNAVAILABLE)
+            "gstCommunicationState", UNAVAILABLE
+        )
         data[BINARY_SENSOR_KEY_CONDUIT_IN_RANGE] = recent_data.setdefault(
-            "conduitInRange", UNAVAILABLE)
+            "conduitInRange", UNAVAILABLE
+        )
         data[BINARY_SENSOR_KEY_CONDUIT_PUMP_IN_RANGE] = recent_data.setdefault(
-            "conduitMedicalDeviceInRange", UNAVAILABLE)
+            "conduitMedicalDeviceInRange", UNAVAILABLE
+        )
         data[BINARY_SENSOR_KEY_CONDUIT_SENSOR_IN_RANGE] = recent_data.setdefault(
-            "conduitSensorInRange", UNAVAILABLE)
+            "conduitSensorInRange", UNAVAILABLE
+        )
 
         # Device info
 
         data[DEVICE_PUMP_SERIAL] = recent_data.setdefault(
-            "medicalDeviceSerialNumber", UNAVAILABLE)
-        data[DEVICE_PUMP_NAME] = (
-            recent_data.setdefault("firstName", "Name") + " " +
-            recent_data.setdefault("lastName", "Unvailable")
+            "medicalDeviceSerialNumber", UNAVAILABLE
         )
-        data[DEVICE_PUMP_MODEL] = recent_data.setdefault(
-            "pumpModelNumber", UNAVAILABLE)
+        data[DEVICE_PUMP_NAME] = (
+            recent_data.setdefault("firstName", "Name")
+            + " "
+            + recent_data.setdefault("lastName", "Unvailable")
+        )
+        data[DEVICE_PUMP_MODEL] = recent_data.setdefault("pumpModelNumber", UNAVAILABLE)
 
         _LOGGER.debug("_async_update_data: %s", data)
 
         return data
 
 
-def getLastMarker(type: str, markers: list) -> dict:
-
-    filteredArray = [marker for marker in markers if marker["type"] == type]
-    sortedArray = sorted(
-        filteredArray,
-        key=lambda x: datetime.strptime(x['dateTime'], "%Y-%m-%dT%H:%M:%S.000-00:00"), reverse=True
+def get_last_marker(marker_type: str, markers: list) -> dict:
+    """Retrieve last marker from type in 24h marker list"""
+    filtered_array = [marker for marker in markers if marker["type"] == marker_type]
+    sorted_array = sorted(
+        filtered_array,
+        key=lambda x: datetime.strptime(x["dateTime"], "%Y-%m-%dT%H:%M:%S.000-00:00"),
+        reverse=True,
     )
 
     try:
-        lastMarker = sortedArray[0]
-        map(lastMarker.pop, ["version", "kind", "index"])
+        last_marker = sorted_array[0]
+        map(last_marker.pop, ["version", "kind", "index"])
 
-        return {"DATETIME": datetime.strptime(lastMarker["dateTime"], "%Y-%m-%dT%H:%M:%S.000-00:00").replace(tzinfo=None), "ATTRS": lastMarker}
+        return {
+            "DATETIME": datetime.strptime(
+                last_marker["dateTime"], "%Y-%m-%dT%H:%M:%S.000-00:00"
+            ).replace(tzinfo=None),
+            "ATTRS": last_marker,
+        }
     except IndexError:
         return None
